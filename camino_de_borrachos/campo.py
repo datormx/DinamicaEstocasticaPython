ab5696e21a54f5ea71e9124e895d086851e0bfaf
Miguel Torres <migueltorresio@outlook.com>
2020-05-20 13:08:50 -0500

    Primera sesión: Conceptos de programación dinámica e inicio de proyecto de Camino de Borrachos donde se ve la práctica de los caminos aleatorios

---
+++ camino_de_borrachos/campo.py
@@ -0,0 +1,19 @@
+
+class Campo:
+
+    def __init__(self):
+        self.coordenadas_de_borrachos = {}
+
+
+    def anadir_borracho(self, borracho, coordenada):
+        self.coordenadas_de_borrachos[borracho] = coordenada #Añade al diccrionario un borracho con sus coordenadas
+
+
+    def mover_borracho(self, borracho):
+        delta_x, delta_y = borracho.camina()
+        coordenada_actual = self.coordenadas_de_borrachos[borracho]
+        nueva_coordenada = coordenada_actual.mover(delta_x, delta_y)
+
+
+    def obtener_coordenada(self, borracho):
+        return self.coordenadas_de_borrachos[borracho]
